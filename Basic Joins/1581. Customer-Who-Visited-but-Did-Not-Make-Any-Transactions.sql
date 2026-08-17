4518eba35fc3ec5bb9f23c81528700d6ab2d9096
stutimahesh <stuti05mahesh@gmail.com>
2026-08-17 17:21:38 +0530

    Add Problem-1581

---
+++ Basic Joins/1581. Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
@@ -0,0 +1,9 @@
+SELECT v.customer_id, COUNT(*) AS count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t
+ON v.visit_id=t.visit_id
+WHERE t.transaction_id is NULL
+GROUP BY v.customer_id
+
+
+
