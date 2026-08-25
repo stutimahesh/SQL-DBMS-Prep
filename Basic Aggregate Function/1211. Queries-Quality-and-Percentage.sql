78ebd037e3b5452e2b91ac6baf9430daa6cc568f
stutimahesh <stuti05mahesh@gmail.com>
2026-08-25 19:18:15 +0530

    Add Problem-1211

---
+++ Basic Aggregate Function/1211. Queries-Quality-and-Percentage.sql
@@ -0,0 +1,7 @@
+SELECT q.query_name,
+        ROUND(AVG(q.rating/q.position),2) AS quality,
+        ROUND(
+            SUM(CASE WHEN q.rating<3 THEN 1 ELSE 0 END)*100/COUNT(q.rating)
+            ,2) AS poor_query_percentage
+FROM Queries as q
+GROUP BY q.query_name
